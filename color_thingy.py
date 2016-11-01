3fff90c55b462e1fb084815692e24b6d1982fd1a
Mike Moss <mrmoss@alaska.edu>
2016-10-31 23:46:47 -0800

    Initial commit.

---
+++ color_thingy.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+import BaseHTTPServer
+import glob
+import json
+import mimetypes
+import os
+import serial
+import SimpleHTTPServer
+import sys
+import threading
+import time
+import urllib
+import urlparse
+
+frame_data=''
+serial_needs_written=False
+serial_mutex=threading.Lock()
+
+deadman_mutex=threading.Lock()
+main_dead=False
+
+def list_serial_ports():
+	if sys.platform.startswith('win'):
+		ports=['COM%s'%(ii+1) for ii in range(256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		ports=glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports=glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
+	serial_ports=[]
+	for port in ports:
+		try:
+			serial_port=serial.Serial(port)
+			serial_port.close()
+			serial_ports.append(port)
+		except(OSError,serial.SerialException):
+			pass
+	return serial_ports
+
+def deadman_check():
+	global deadman_mutex
+	global main_dead
+	deadman_mutex.acquire()
+	is_dead=main_dead
+	deadman_mutex.release()
+	return is_dead
+
+def serial_handler():
+	while True:
+		try:
+			if deadman_check():
+				break
+			serial_port=None
+			serial_ports=list_serial_ports()
+			for port in serial_ports:
+				if port.lower().find('bluetooth')<0:
+					serial_port=serial.Serial(port=port,baudrate=115200)
+					if serial_port.isOpen():
+						print('opened '+port)
+					while serial_port.isOpen():
+						serial_port.inWaiting()
+						if deadman_check():
+							break
+						data=read_serial_data()
+						if not data[0]:
+							continue
+						length=serial_port.write(data[1])
+						if length==len(data[1]):
+							print('sent data '+str(len(data[1])))
+							clear_serial_data()
+			time.sleep(1)
+		except IOError as error:
+			print('disconnected')
+		except Exception as error:
+			print(error)
+
+def send_serial_data(data):
+	global frame_data
+	global serial_needs_written
+	global serial_mutex
+	serial_mutex.acquire()
+	frame_data=data
+	serial_needs_written=True
+	serial_mutex.release()
+
+def read_serial_data():
+	global frame_data
+	global serial_needs_written
+	global serial_mutex
+	serial_mutex.acquire()
+	ret_data=(serial_needs_written,frame_data)
+	serial_mutex.release()
+	return ret_data
+
+def clear_serial_data():
+	global frame_data
+	global serial_needs_written
+	global serial_mutex
+	serial_mutex.acquire()
+	serial_needs_written=False
+	frame_data=''
+	serial_mutex.release()
+
+class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			self.path=self.path.split('?')
+			query_str='?'.join(self.path[1:])
+			if len(query_str)>0:
+				query_str='?'+query_str
+			self.path=self.path[0]
+			if len(self.path)>0 and self.path[-1]=='/':
+				self.path+='index.html'
+			cwd=os.getcwd()+'/web/'
+			self.path=os.path.abspath(cwd+self.path)
+			if self.path.find(cwd)!=0 or not os.path.isfile(self.path):
+				self.send_response(404)
+				self.end_headers()
+				return
+			file=open(self.path,'r')
+			self.send_response(200)
+			mime=mimetypes.guess_type(self.path)
+			if len(mime)>0:
+				mime=mime[0]
+			else:
+				mime='text/plain'
+			print(mime+' '+self.path)
+			self.send_header('Content-type',mime)
+			self.end_headers()
+			self.wfile.write(file.read())
+			self.wfile.close()
+		except Exception as error:
+			self.send_response(401)
+
+	def do_POST(self):
+		try:
+			data_len=int(self.headers.getheader('content-length',0))
+			if data_len>2e6:
+				self.send_response(413)
+				self.end_headers()
+			try:
+				data=self.rfile.read(data_len)
+				data=json.loads(data)
+				data_bytes=''
+				if not isinstance(data,list):
+					raise Exception('Not a list.')
+				if len(data)!=8:
+					raise Exception('Invalid frame.')
+				for yy in range(0,len(data)):
+					if not isinstance(data[yy],list):
+						raise Exception('Invalid frame.')
+					if len(data[yy])!=8:
+						raise Exception('Invalid frame.')
+					for xx in range(0,len(data[yy])):
+						if not isinstance(data[yy][xx],list):
+							raise Exception('Invalid frame.')
+						if len(data[yy][xx])!=3:
+							raise Exception('Invalid frame.')
+						for ii in range(0,len(data[yy][xx])):
+							if data[yy][xx][ii]<0 or data[yy][xx][ii]>255:
+								raise Exception('Invalid frame.')
+							data_bytes+=chr(data[yy][xx][ii])
+				send_serial_data(data_bytes)
+			except Exception as error:
+				print(error)
+				self.send_response(400)
+				self.end_headers()
+				return
+			self.send_response(200)
+			self.send_header('Content-type','application/javascript')
+			self.end_headers()
+			self.wfile.close()
+		except Exception as error:
+			self.send_response(401)
+			self.end_headers()
+
+if __name__=='__main__':
+	try:
+		thread=threading.Thread(target=serial_handler)
+		thread.start()
+		Handler=MyHandler
+		server=BaseHTTPServer.HTTPServer(('127.0.0.1',8080),MyHandler)
+		server.serve_forever()
+	except Exception as error:
+		print('ERROR - '+str(error))
+	except KeyboardInterrupt:
+		print('interrupt')
+		deadman_mutex.acquire()
+		main_dead=True
+		deadman_mutex.release()
---
+++ web/js/color_thingy.js
@@ -0,0 +1,183 @@
+function color_thingy_t(canvas,picker,sender)
+{
+	this.canvas=canvas;
+	this.picker=picker;
+	this.sender=sender;
+	this.ctx=canvas.getContext('2d');
+	this.border_size=1;
+	this.canvas.style.border='black solid '+this.border_size+'px';
+	this.x_cells=8;
+	this.y_cells=8;
+	this.is_down=false;
+	this.frame=
+	[
+		[[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0]],
+		[[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0]],
+		[[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0]],
+		[[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0]],
+		[[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0]],
+		[[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0]],
+		[[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0]],
+		[[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0],[0,255,0],[255,0,0]]
+	];
+
+	var _this=this;
+	window.addEventListener('resize',function(evt){_this.resize(evt);});
+	window.addEventListener('orientationchange',function(evt){_this.resize(evt);});
+
+	this.sender.addEventListener('click',function(evt)
+	{
+		if(_this.valid_frame())
+		{
+			_this.xhr(JSON.stringify(_this.frame),function(success)
+			{
+				console.log(success);
+			},
+			function(error)
+			{
+				console.log(error);
+			});
+		}
+	});
+
+	this.canvas.addEventListener('mousedown',function(evt){_this.set_down(evt);});
+	this.canvas.addEventListener('touchstart',function(evt){_this.set_down(evt);});
+	window.addEventListener('mouseup',function(evt){_this.set_up(evt);});
+	window.addEventListener('touchend',function(evt){_this.set_up(evt);});
+	window.addEventListener('touchcancel',function(evt){_this.set_up(evt);});
+	window.addEventListener('touchleave',function(evt){_this.set_up(evt);});
+	this.canvas.addEventListener('mousemove',function(evt){_this.draw(evt);});
+	this.canvas.addEventListener('touchmove',function(evt){_this.draw(evt);});
+
+	this.resize();
+}
+
+color_thingy_t.prototype.calc_canvas=function()
+{
+	var props=
+	{
+		x:this.canvas.offsetLeft,
+		y:this.canvas.offsetTop,
+		width:this.canvas.offsetWidth-this.border_size*2,
+		height:this.canvas.offsetHeight-this.border_size*2,
+		cell_w:this.canvas.offsetWidth/this.x_cells,
+		cell_h:this.canvas.offsetHeight/this.y_cells
+	};
+	return props;
+}
+
+color_thingy_t.prototype.resize=function()
+{
+	this.canvas.style.width='60%';
+	this.canvas.style.width=this.canvas.style.height=this.canvas.height=
+		this.canvas.width=this.canvas.offsetWidth;
+	this.draw_frame();
+}
+
+color_thingy_t.prototype.set_down=function(evt)
+{
+	this.is_down=true;
+	this.draw(evt);
+}
+
+color_thingy_t.prototype.set_up=function()
+{
+	this.is_down=false;
+	this.draw_frame();
+}
+
+color_thingy_t.prototype.draw=function(evt)
+{
+	if(!this.is_down||(evt.touches&&evt.touches.length==0))
+		return false;
+	if(evt.touches)
+		evt=evt.touches[0];
+	var props=this.calc_canvas();
+	var x=Math.floor((window.scrollX+evt.clientX-props.x+this.border_size)/props.cell_w);
+	var y=Math.floor((window.scrollY+evt.clientY-props.y+this.border_size)/props.cell_h);
+	if(x>=this.x_cells)
+		x=this.x_cells-1;
+	if(x<0)
+		x=0;
+	if(y>=this.y_cells)
+		y=this.y_cells-1;
+	if(y<0)
+		y=0;
+	this.frame[y][x]=this.rgb_from_html(this.picker.value);
+	this.draw_frame();
+}
+
+color_thingy_t.prototype.valid_frame=function()
+{
+	if(this.frame.length!=this.y_cells)
+		return false;
+	for(var yy=0;yy<this.y_cells;++yy)
+	{
+		if(this.frame[yy].length!=this.x_cells)
+			return false;
+		for(var xx=0;xx<this.x_cells;++xx)
+			if(this.frame[yy][xx].length!=3)
+				return false;
+	}
+	return true;
+}
+
+color_thingy_t.prototype.rgb_arr_to_css=function(col)
+{
+	return 'rgb('+col[0]+','+col[1]+','+col[2]+')';
+}
+
+color_thingy_t.prototype.rgb_from_html=function(col)
+{
+	if(col.length!=7||col[0]!='#')
+		return [0,0,0];
+	var rgb_col=
+	[
+		parseInt(col.substr(1,2),16),
+		parseInt(col.substr(3,2),16),
+		parseInt(col.substr(5,2),16)
+	];
+	return rgb_col;
+}
+
+color_thingy_t.prototype.draw_frame=function()
+{
+	if(!this.valid_frame())
+	{
+		console.log('Invalid frame!');
+		return;
+	}
+	var props=this.calc_canvas();
+	this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+	for(var yy=0;yy<this.y_cells;++yy)
+		for(var xx=0;xx<this.x_cells;++xx)
+		{
+			this.ctx.strokeStyle=this.ctx.fillStyle=this.rgb_arr_to_css(this.frame[yy][xx]);
+			this.ctx.fillRect(xx*props.cell_w,yy*props.cell_h,
+				props.cell_w,props.cell_h);
+			this.ctx.strokeRect(xx*props.cell_w,yy*props.cell_h,
+				props.cell_w,props.cell_h);
+		}
+}
+
+color_thingy_t.prototype.xhr=function(data,onsuccess,onerror)
+{
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function()
+	{
+		if(xmlhttp.readyState==4)
+		{
+			if(xmlhttp.status==200)
+			{
+				if(onsuccess)
+					onsuccess(xmlhttp.responseText);
+			}
+			else
+			{
+				onerror('Response '+xmlhttp.status);
+			}
+		}
+	};
+	xmlhttp.open('POST','',true);
+	xmlhttp.send(data);
+}
